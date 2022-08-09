5098bd5d7b2f34603b129b8622f28562b09ca379
Gabriel Cari <g.cari@hotmail.com>
2022-08-09 14:59:04 -0300

    Fix: Icons redirecting to corresponding page.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,17 +1,24 @@
 import * as React from 'react';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
 import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import RestoreIcon from '@mui/icons-material/Restore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import HomeIcon from '@mui/icons-material/Home';
-import styled from 'styled-components';
-import { useContext } from 'react';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+
 import { PageNavigationContext } from '../contexts/PageNavigationContext';
+
 export default function Navigation() {
-    const {page: value, setPage: setValue} = useContext(PageNavigationContext);
+    const navigate = useNavigate();
+    function navigateTo(url) {
+        navigate(url);
+    }
+
+    const { page: value, setPage: setValue } = useContext(PageNavigationContext);
     return (
         <BoxStyled sx={{ width: "100%", fontSize: "100px" }}>
             <BottomNavigation
@@ -21,10 +28,9 @@ export default function Navigation() {
                     setValue(newValue);
                 }}
             >
-                <BottomNavigationAction label="Home" icon={<HomeIcon />} />
-                <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-                <BottomNavigationAction label="My locations" icon={<LocationOnIcon />} />
-                {/* <BottomNavigationAction label="Nearby" icon={<AccountBoxIcon />} /> */}
+                <BottomNavigationAction label="Home" icon={<HomeIcon />} onClick={() => navigateTo("/")} />
+                <BottomNavigationAction label="Rent" icon={<AddBoxIcon />} onClick={() => navigateTo("/new-location")} />
+                <BottomNavigationAction label="My locations" icon={<LocationOnIcon />} onClick={() => navigateTo("/my-locations")} />
             </BottomNavigation>
         </BoxStyled>
     );
@@ -32,7 +38,6 @@ export default function Navigation() {
 
 const BoxStyled = styled(Box)`
     &&{
-        color: red;
         @media (min-width: 1200px) {
             width: 650px;
             margin-right: 200px;
